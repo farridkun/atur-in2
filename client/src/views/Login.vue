9e72bb3131ef45cc7ddae6b5ceb09a0ea214ea3e
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2020-01-30 01:47:49 +0700

    card login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,57 +2,49 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <v-form
-          v-if="!loading"
-          v-model="valid"
-          @submit.prevent="login({ valid, user })"
-          @keydown.prevent.enter>
-          <v-text-field
-            v-model="user.username"
-            :rules="notEmptyRules"
-            label="Username"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="user.password"
-            :rules="notEmptyRules"
-            label="Password"
-            type="password"
-            required
-          ></v-text-field>
-          <v-btn type="submit" :disabled="!valid">Login</v-btn>
-        </v-form>
-        <v-progress-circular
-          v-if="loading"
-          :size="70"
-          :width="7"
-          indeterminate
-          color="primary">
-        </v-progress-circular>
+        <v-card :elevation="hover ? 24 : 6" class="mx-auto pa-6">
+          <v-form
+            v-if="!loading"
+            v-model="valid"
+            @submit.prevent="login({ valid, user })"
+            @keydown.prevent.enter
+          >
+            <v-text-field v-model="user.username" :rules="notEmptyRules" label="Username" required></v-text-field>
+            <v-text-field
+              v-model="user.password"
+              :rules="notEmptyRules"
+              label="Password"
+              type="password"
+              required
+            ></v-text-field>
+            <v-btn type="submit" :disabled="!valid">Login</v-btn>
+          </v-form>
+        </v-card>
+        <v-progress-circular v-if="loading" :size="70" :width="7" indeterminate color="primary"></v-progress-circular>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-import { notEmptyRules } from '@/validators';
+import { mapState, mapActions } from "vuex";
+import { notEmptyRules } from "@/validators";
 
 export default {
-  name: 'login',
+  name: "login",
   data: () => ({
     valid: false,
     user: {
-      username: '',
-      password: '',
+      username: "",
+      password: ""
     },
-    notEmptyRules,
+    notEmptyRules
   }),
   computed: {
-    ...mapState('auth', { loading: 'isAuthenticatePending' }),
+    ...mapState("auth", { loading: "isAuthenticatePending" })
   },
   methods: {
-    ...mapActions('localAuth', ['login']),
-  },
+    ...mapActions("localAuth", ["login"])
+  }
 };
 </script>
